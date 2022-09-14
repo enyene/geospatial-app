7017b0c197bd4cf5e4255aac86f3d90d6c55c86a
Enyene Akpan <akpanenyene@gmail.com>
2022-09-14 12:28:00 +0100

    add map app

--- geospatial/settings.py
+++ geospatial/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'maps'
+    'map'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = []
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- map/views.py
+++ map/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import os 
+import folium
 
 # Create your views here.
+def home(request):
+    shp_dir = os.path.join(os.getcwd(),'media','shp')
+    # folium
+    m = folium.Map(location=[-16.22,-71.59],zoom_start=10)
+    ## style
+    style_basin = {'fillColor': '#228B22', 'color': '#228B22'}
+    style_rivers = { 'color': 'blue'}
+    ## adding to view
+    folium.GeoJson(os.path.join(shp_dir,'basin.geojson'),name='basin',style_function=lambda x:style_basin).add_to(m)
+    folium.GeoJson(os.path.join(shp_dir,'rivers.geojson'),name='rivers',style_function=lambda x:style_rivers).add_to(m)
+    folium.LayerControl().add_to(m)
+    ## exporting
+    m=m._repr_html_()
+    context = {'my_map': m}
+    return render(request,'map/home.html',context)
